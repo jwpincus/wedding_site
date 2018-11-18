2b07eb3e77029ad9b9afaf81ef9d1108e08f4c60
Jack Pincus <jwpincus@gmail.com>
2018-11-18 11:47:50 -0800

    update guest view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,18 +1,18 @@
 class AdminController < ApplicationController
   before_action :is_admin
-  
+
   def index
-    @guests = Guest.all
+    @guests = Guest.where(guest_id: nil)
     @messages = Message.all
   end
-  
+
   private
-  
+
   def is_admin
     @current_user = current_user
     if !@current_user || !@current_user.is_admin
       redirect_to root_path
     end
   end
-  
+
 end
