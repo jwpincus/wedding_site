0c35fcb13952bb6dbded5d619f6a3ce69a9e2736
Jack Pincus <jwpincus@gmail.com>
2019-01-10 15:15:22 -0800

    info updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.default_url_options[:host] = 'https://www.cayleyandjack.com'
   root to: 'home#index'
+  get '/accommodations',  to: 'home#accommodations'
+  get '/squamish',        to: 'home#squamish'
+  get '/event',           to: 'home#event'
 
-  get '/signup', to: 'users#new'
-  post '/users', to: 'users#create'
+  # get '/signup', to: 'users#new'
+  # post '/users', to: 'users#create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
